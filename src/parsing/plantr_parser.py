e233bccacb0811ddf7c99fa7b51904c50c963c49
Max Worsley <maxworsley@outlook.com>
2023-10-04 19:37:49 +0100

    Implemented parsing variable assignment

---
+++ src/parsing/plantr_parser.py
@@ -0,0 +1,53 @@
+import sys
+import re
+
+def is_variable(token):
+    """Check if the token is a variable"""
+    return re.match(r'\[[a-zA-Z0-9]+\]', token) is not None
+
+def is_number(token):
+    """Check if the token is a number"""
+    return re.match(r'\d+', token) is not None
+
+def parse_tokens(tokens, variables):
+    """parse the tokens, doing the actions described where [var] indicates a variable, # indicates addition and ~ indicates subtraction"""
+
+    if len(tokens) == 0:
+        return
+
+    focus = tokens[0]
+
+    if is_variable(focus):
+        variable_name  = focus[1:-1]
+        if len(tokens) == 1:
+            return variables[variable_name]
+        elif tokens[1] == 'is':
+            variables[variable_name] = parse_tokens(tokens[2:], variables)
+            print(f"Updated variable {variable_name} with value {variables[variable_name]}")
+        else:
+            raise ValueError('Incorrect input - expected "is"')
+    elif is_number(focus):
+        return int(focus)
+    else:
+        raise ValueError('Incorrect input - expected variable or number')
+        
+    
+
+def parse_lines(lines):
+    variables = {}
+    for line in lines:
+        tokens = line.split(' ')
+        
+        parse_tokens(tokens, variables)
+
+
+def get_lines(file_name):
+        """Read the file and return a list of lines"""
+        with open(file_name, 'r') as file:
+            return file.readlines()
+
+if __name__ == "__main__":
+    file_name = sys.argv[1]
+
+    lines = get_lines(file_name)
+    parse_lines(lines)
---
+++ src/parsing/test_parser.py
@@ -0,0 +1,23 @@
+import unittest
+
+from plantr_parser import parse_tokens
+
+
+class TestParser(unittest.TestCase):
+    def test_is(self):
+        vars = dict()
+        parse_tokens(['[pot]', 'is', '4'], vars)
+        self.assertEqual(vars['pot'], 4)
+
+    def test_is_to_is(self):
+        vars = dict()
+        parse_tokens(['[pot1]', 'is', '4'], vars)
+        parse_tokens(['[pot2]', 'is', '[pot1]'], vars)
+        self.assertEqual(vars['pot2'], 4)
+    
+    def test_error(self):
+        vars = dict()
+        with self.assertRaises(ValueError):
+            parse_tokens(['[pot]', 'is', 'a'], vars)
+
+    
