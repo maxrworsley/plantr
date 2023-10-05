f282fdb61824aa700ab3dcf83b048f01965622b8
Max Worsley <maxworsley@outlook.com>
2023-10-05 19:23:53 +0100

    Switch to consumer

--- src/parsing/plantr_parser.py
+++ src/parsing/plantr_parser.py
@@ -1,6 +1,21 @@
 import sys
 import re
 
+class TokenList:
+    def __init__(self, tokens) -> None:
+        self.tokens = tokens
+        self.index = 0
+
+    def peek(self):
+        return self.tokens[self.index]
+
+    def consume(self):
+        self.index += 1
+        return self.tokens[self.index - 1]
+    
+    def __len__(self):
+        return len(self.tokens) - self.index
+
 def raise_parse_error(variables, remaining, error_message):
     """Raise a parse error and print out the variables and remaining tokens"""
     print('Parse error')
@@ -10,46 +25,40 @@ def raise_parse_error(variables, remaining, error_message):
     print(remaining)
     raise ValueError(f'Parse error - {error_message}')
 
-def is_variable(token):
-    return re.match(r'\[[a-zA-Z0-9]+\]', token) is not None
+is_variable = lambda x: re.match(r'\[[a-zA-Z0-9]+\]', x) is not None
+is_number = lambda x: re.match(r'\d+', x) is not None
+is_show = lambda x: x == 'show'
+is_is = lambda x: x == 'is'
+is_add = lambda x: x == '#'
+is_subtract = lambda x: x == '~'
 
-def is_number(token):
-    return re.match(r'\d+', token) is not None
-
-def is_show(token):
-    return token == 'show'
-
-def is_add(token):
-    return token == '#'
-
-def is_subtract(token):
-    return token == '~'
-
-def parse_tokens(tokens, variables):
+def parse_tokens(tokens: TokenList, variables):
     """parse the tokens, doing the actions described where [var] indicates a variable, # indicates addition and ~ indicates subtraction"""
 
     if len(tokens) == 0:
         raise_parse_error(variables, tokens, 'expected variable or number')
 
-    focus = tokens[0]
+    focus = tokens.consume()
 
     if is_variable(focus):
-        variable_name  = focus[1:-1]
+        variable_name = focus[1:-1]
         if len(tokens) == 1:
             return variables[variable_name]
-        elif tokens[1] == 'is':
-            variables[variable_name] = parse_tokens(tokens[2:], variables)
+        elif is_is(tokens.peek()):
+            tokens.consume()
+            variables[variable_name] = parse_tokens(tokens, variables)
         else:
             raise raise_parse_error(variables, tokens, 'expected "is"')
     elif is_show(focus):
-        print(parse_tokens(tokens[1:], variables))
+        print(parse_tokens(tokens, variables))
     elif is_number(focus):
         if len(tokens) == 1:
             return int(focus)
-        elif is_add(tokens[1]):
-            return int(focus) + parse_tokens(tokens[2:], variables)
-        elif is_subtract(tokens[1]):
-            return int(focus) - parse_tokens(tokens[2:], variables)
+        next_token = tokens.consume()
+        if is_add(next_token):
+            return int(focus) + parse_tokens(tokens, variables)
+        elif is_subtract(next_token):
+            return int(focus) - parse_tokens(tokens, variables)
         else:
             raise raise_parse_error(variables, tokens, 'expected # or ~')
     else:
@@ -59,9 +68,9 @@ def parse_tokens(tokens, variables):
 def parse_lines(lines):
     variables = {}
     for line in lines:
-        tokens = line.split(' ')
+        token_list = TokenList(line.split(' '))
         
-        parse_tokens(tokens, variables)
+        parse_tokens(token_list, variables)
 
 
 def get_lines(file_name):
