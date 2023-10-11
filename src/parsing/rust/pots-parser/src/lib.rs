e13fb9a105afbddaf3ef0858cc56bf4d8668f5c6
Max Worsley <maxworsley@outlook.com>
2023-10-11 20:43:12 +0100

    Add doctest to main parsing function

--- src/parsing/rust/pots-parser/src/lib.rs
+++ src/parsing/rust/pots-parser/src/lib.rs
@@ -5,7 +5,19 @@ mod parse_show;
 mod parse_wait;
 mod vars;
 
-
+/// Parses a plantr program
+/// 
+/// # Example
+/// ```
+/// use pots_parser;
+/// 
+/// let input = "grow plant1 for 3;\nwait for 1;\nshow plant1;";
+/// pots_parser::parse_plantr(input);
+/// ```
+/// 
+/// # Panics
+/// 
+/// Panics if the input is invalid
 pub fn parse_plantr(input: &str) {
     use parse_functions::parse_statement;
 
